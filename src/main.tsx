44d3c83f9524b9af509625ed863f318cb00461f1
Dan Avner <ldpavner@gmail.com>
2026-07-31 13:01:32 +0000

    feat(theme): add Merriweather and Fira Code fonts from Solar Dusk

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import '@fontsource-variable/oxanium'
+import '@fontsource-variable/merriweather'
+import '@fontsource-variable/fira-code'
 import './index.css'
 import App from './App.tsx'
 
